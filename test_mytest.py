01a42792bdb11a4f900d4b1efe68b0428bc9319f
yuelan.pan <960272259@qq.com>
2020-04-01 21:51:51 +0800

    new code

--- test_mytest.py
+++ test_mytest.py
@@ -1,2 +1,14 @@
-import  unittest
-ß
+import requests
+
+
+def test_get_homepage():
+    url = "https://mubu.com/"
+    headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/80.0.3987.149 Safari/537.36 "
+               }
+    resp = requests.get(url, headers=headers, verify=False)
+    assert resp.status_code == 200
+
+
+if __name__ == "__main__":
+    test_get_homepage()
