dce2a94bc5cf8f9587ebbfcd3f89f3c202a536a3
yuelan.pan <960272259@qq.com>
2020-04-05 20:07:17 +0800

    add jsonpath

---
+++ my_httprunner/runner.py
@@ -0,0 +1,28 @@
+import jsonpath
+
+from my_httprunner.loader import load_yaml
+import requests
+
+
+def run_yaml(file_path):
+    load_json = load_yaml(file_path)
+
+    request = load_json["request"]
+    method = request.pop("method")
+    url = request.pop("url")
+    kwargs = request
+    reps = requests.request(method=method, url=url, **kwargs)
+
+    validator_mapping = load_json["validate"]
+    for key in validator_mapping:
+        if "$" in key:
+           actual_value = extract_json_field(reps, key)
+        else:
+           actual_value = getattr(reps, key)
+        expected_value = validator_mapping[key]
+        return actual_value == expected_value
+
+
+def extract_json_field(resp, json_field):
+    value = jsonpath.jsonpath(resp.json(), json_field)
+    return value
--- tests/test_single_api.py
+++ tests/test_single_api.py
@@ -1,14 +1,18 @@
 import os
 import unittest
 from my_httprunner.loader import load_yaml
+from my_httprunner.runner import run_yaml
 
 
 class TestSingleApi(unittest.TestCase):
 
-    def test_loading_single_api(self):
+    def test_run_single_api_yaml(self):
         single_api_yaml = os.path.join(os.path.dirname(__file__), "api", "homepage.yml")
-        loaded_json = load_yaml(single_api_yaml)
-        return loaded_json
+        resp = run_yaml(single_api_yaml)
+        self.assertEqual(resp, True)
 
-    def test_single_api(self):
-        pass
+    def test_run_single_with_jsonpath(self):
+        single_api_yaml = os.path.join(os.path.dirname(__file__), "api", "login.yml")
+        resp = run_yaml(single_api_yaml)
+        self.assertEqual(resp, True)
+
